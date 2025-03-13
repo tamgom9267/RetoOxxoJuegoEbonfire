fc3dd0c2835a1654fdc870284bba114f3907305d
Santiago Tamez <stamez26@icloud.com>
2025-03-12 18:02:22 -0600

    Creación de Menu y Login con navegación funcional

---
+++ Assets/MenuLogin/Scripts/LoginController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginController : MonoBehaviour
+{
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void GoToLaberinto()
+    {
+        SceneManager.LoadScene("LaberintoScene");
+    }
+
+    public void GoToTaberna()
+    {
+        SceneManager.LoadScene("TabernaScene");
+    }
+
+    public void GoToDecision()
+    {
+        SceneManager.LoadScene("DecisionScene");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
