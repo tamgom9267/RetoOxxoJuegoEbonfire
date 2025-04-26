6351c10b07dcde1adf78c5dae89025bd7e3e00a0
A01723266 <A01723266@tec.mx>
2025-04-26 13:25:15 -0600

    Casos de Prueba Para Juego Taberna
    Caso 1 se activa con (Ctrl + 1)
    Caso 2 se activa con (Ctrl + 2)

---
+++ Assets/Taberna/Scripts/CasoDePrueba.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CasoDePrueba : MonoBehaviour
+{
+    void Update()
+    {
+        // Opción 1: Ctrl + 1
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActivarDebug(127.53f, 15, 5);
+        }
+
+        // Opción 2: Ctrl + 2
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ActivarDebug(45.31f, 6, 16);
+        }
+    }
+
+    void ActivarDebug(float tiempoTotal, int turnosJugador, int vidaJugador)
+    {
+        // Activar 4 de los 5 objetos (dejando 'objeto_Chips' sin activar)
+        PlayerPrefs.SetInt("objeto_Coffe", 1);
+        PlayerPrefs.SetInt("objeto_Chocolate_Bar", 1);
+        PlayerPrefs.SetInt("objeto_Bead", 1);
+        PlayerPrefs.SetInt("objeto_Milk", 1);
+        // No tocar objeto_Chips
+
+        // Definir los nuevos valores extra
+        PlayerPrefs.SetFloat("tiempo_total", tiempoTotal);
+        PlayerPrefs.SetInt("turnos_jugador", turnosJugador);
+        PlayerPrefs.SetInt("vida_jugador", vidaJugador);
+
+        PlayerPrefs.Save(); // Guardar cambios
+
+        // Recargar escena para aplicar cambios
+        SceneManager.LoadScene("Taberna Scene");
+    }
+}
